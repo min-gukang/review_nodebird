b91df236c53164fded3299ae7840a4ab226f0238
min-gukang <route2054@gmail.com>
2021-03-02 13:35:00 +0900

    install eslint

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+    "env": { // 프로젝트의 사용 환경을 설정한다.
+        "browser": true,
+        "commonjs": true,
+        "es2021": true
+    },
+    "extends": "eslint:recommended", // 다른 ESLint 설정을 확장해서 사용할때 설정한다
+    "parserOptions": {
+        "ecmaVersion": 12
+    },
+    "rules": {
+    }
+};
